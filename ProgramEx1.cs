6360c33ce588ee37d8545d2dfc085a66ec3c489b
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 19:10:16 -0300

    Add files via upload

---
+++ ProgramEx1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex1
+{
+    internal class Retangulo
+    {
+        
+        private double h;
+        private double b;
+        private double a;
+
+        public void seth(double ba)
+        {
+            h = ba;
+
+        }
+        public void setb(double ba)
+        {
+            b = ba;
+
+        }
+        public double geth()
+        {
+            return h;
+        }
+        public double getb()
+        {
+            return b;
+        }
+
+        public double geta()
+        {
+            return a;
+        }
+
+        public void calcular()
+        {
+            a = b * h;
+        }
+
+
+    }
+
+}
